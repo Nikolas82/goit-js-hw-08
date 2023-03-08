22fcebf0623eb643be9af6728964051d8769a481
Nikolas82 <nikobogdan34@gmail.com>
2023-03-08 10:25:02 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,21 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
+// console.log(player)
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+function onPlay(time) {
+  localStorage.setItem(STORAGE_KEY, time.seconds);
+}
 
+player.on('timeupdate', throttle(onPlay, 1000));
 
-
-
-
-
-    
+// console.log(localStorage.getItem(STORAGE_KEY))
+if (localStorage.getItem(STORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
